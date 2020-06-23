03e2423e5112ef2a2e6b0c3a6e484b98f2b604ea
juqi <1928747053@qq.com>
2020-06-23 14:29:37 +0800

    springboot第一次项目

---
+++ src/main/java/com/igeek/HelloSpringApplication.java
@@ -0,0 +1,15 @@
+package com.igeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+启动类
+ */
+@SpringBootApplication
+public class HelloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class,args);
+    }
+}
